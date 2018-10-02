e1a8cbb18e7d84489090f57865806ee7f50b9fe4
GFabien <fabien.girka@supelec.fr>
2018-10-02 15:11:49 +0200

    Erase some useless parts

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-const wd = require("word-definition");
 const Parser = require('./parser');
 
 /**
 
 const http = require('http');
-
-function demo4() {
-	wd.getDef("garçon", "fr", { hyperlinks: "html", formatted: true }, function(result) {
-        // print(result, "Definition of 'GARCON', french, with HTML hyperlinks and text formatting");
-        return result;
-	});
-}
 
 const server = http.createServer(function(req, res) {
 
