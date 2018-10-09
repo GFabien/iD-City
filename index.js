938b3aab29fcefefcf21c9d8c8a5efb11a59365f
GFabien <fabien.girka@supelec.fr>
2018-10-09 09:48:20 +0200

    Rewrite parser with observables and work on filtering articles from BDD

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const Parser = require('./parser');
+const parser = require('./parser');
+const articles = require('./pick-articles');
+const Rx = require('rxjs');
 
 /**
 
@@ -20,9 +22,33 @@ server.listen(8080); // Démarre le serveur
 console.log("j'ecoute sur 8080");
 
  */
+/*
+console.log(articles[0].name._cdata);
+const filtered_articles = articles.filter(x => x.description._cdata.toLowerCase().includes('entretien'));
+console.log(filtered_articles.length);
+*/
 
-monParser = new Parser.parser('entrée', 'fr', function(result) {
 
-    console.log(result.categories);
+const obs = parser('entretien', 'fr');
+obs.subscribe((result) => {
+    // console.log(result.categories[0]);
+    const words = result.categories[0].words;
+    words.push('entretien');
+    //console.log(words);
+    console.log(articles(words).length);
 });
-monParser.getTitles();
+
+/*
+const obs2 = Rx.Observable.create(function subscribe(observer) {
+    observer.next(parser('fromage', 'fr'));
+    observer.next(parser('jambon', 'fr'));
+})
+
+const resultats = [];
+obs2.subscribe((result) => {
+    result.subscribe((x) => {
+        resultats.push(x);  
+        console.log(resultats);
+    });
+})
+*/
--- pick-articles.js
+++ pick-articles.js
@@ -6,4 +6,42 @@ const options = {
     alwaysChildren: true,
     compact: true
 };
+
 const articles = convert.xml2js(xml, options).datas.data;
+
+const relevantKeys = ['name', 'topic', 'objective', 'description'];
+
+function internal_filter(article, array, key) {
+    return (article[key]._cdata && array.some((element) => {
+        return article[key]._cdata.toLowerCase().includes(element);
+    })); 
+}
+
+function internal_filter2(article, array, keys) {
+    return (
+        keys.reduce((currentValue, key) => {
+            // console.log('key', key);
+            // console.log('currentValue', currentValue);
+            (article[key]._cdata && array.some((element) => {
+                return article[key]._cdata.toLowerCase().includes(element);
+            })) + currentValue;
+        }, 0)
+    ); 
+}
+
+function filter_articles(words) {
+    return articles.filter(article => 
+        internal_filter(article, words, 'name')  ||
+        internal_filter(article, words, 'topic')  ||
+        internal_filter(article, words, 'objective')  ||
+        internal_filter(article, words, 'description')
+    );
+}
+
+function filter_articles(words) {
+    return articles.filter(article => 
+        internal_filter2(article, words, relevantKeys) > 0
+    );
+}
+
+module.exports = filter_articles;
